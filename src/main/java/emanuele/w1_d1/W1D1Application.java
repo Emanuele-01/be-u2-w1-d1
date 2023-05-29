b27609b3e8e2660ac3f7cba4874e8805adffbc43
Emanuele-01 <emanuele.pitoni.2001@gmail.com>
2023-05-29 14:15:54 +0200

    initial commit

---
+++ src/main/java/emanuele/w1_d1/W1D1Application.java
@@ -0,0 +1,13 @@
+package emanuele.w1_d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W1D1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W1D1Application.class, args);
+	}
+
+}
